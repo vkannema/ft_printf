ae0d468d74deb499a2852b04ac1517287ea39b49
Victor KANNEMACHER <vkannema@e1r3p19.42.fr>
2017-01-03 17:12:56 +0100

    typeflags

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vkannema <vkannema@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 13:20:34 by vkannema          #+#    #+#             */
-/*   Updated: 2016/12/28 12:45:42 by vkannema         ###   ########.fr       */
+/*   Updated: 2017/01/03 16:02:01 by vkannema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@
 int	main()
 {
 	int	a;
+	int *b;
 
-	a = 10;
-	printf("%d\n\n", printf("%%\n"));
-	ft_printf("%d\n\n", ft_printf("%%\n"));
+	a = 11442;
+	b = &a;
+	printf("%d\n\n", printf("%s\n", "coucou bryan"));
+	ft_printf("%d\n\n", ft_printf("%s\n", "coucou bryan"));
 	return (0);
 }
